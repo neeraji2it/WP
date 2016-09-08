aadffb7840dfcc40e93bd2f189d31fa297f73edc
Rails <railsG>
2016-09-08 19:16:53 +0530

    some more cheanges

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,8 @@ class Note < ActiveRecord::Base
   validates :category_id, :name, :body,:image, :presence => true
 has_attached_file :image
   validates_attachment_content_type :image,:content_type => [ 'image/jpeg','image/jpg', 'image/png', 'image/gif','image/bmp', 'image/x-png', 'image/pjpeg' ]
+def self.search(search)
+  # Title is for the above case, the OP incorrectly had 'name'
+  where("name LIKE ?", "%#{search}%")
+end
 end
